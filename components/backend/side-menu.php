105536ba24ff4a45d7ad65069240489424e52a15
panuwit169 <pra169@hotmail.com>
2017-08-12 13:18:33 +0700

    backend success

--- components/backend/side-menu.php
+++ components/backend/side-menu.php
@@ -4,9 +4,17 @@
             <li>
                 <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
             </li>
+
             <li>
-                <a href="order.php"><i class="fa fa-shopping-cart fa-fw"></i> รายการสั่งซื้อ</a>
+                <?php
+                  $sql ="SELECT COUNT(*) FROM orders JOIN payment ON orders.order_id = payment.order_id WHERE payment.status = 'notcheck' ORDER BY payment.id ASC";
+                  $result = mysql_query($sql,$con);
+                  while($rs = mysql_fetch_array($result)){
+                ?>
+                <a href="order.php"><i class="fa fa-shopping-cart fa-fw"></i> รายการสั่งซื้อ <span class="badge" style="float:right"><?php echo $rs[0];?></span></a>
+                <?php } ?>
             </li>
+
             <li>
                 <a href="product.php"><i class="fa fa-table fa-fw"></i> จัดการสินค้า</a>
             </li>
