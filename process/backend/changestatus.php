ad731cca085bda478f5c2b8037bccbdb2da1273e
panuwit169 <pra169@hotmail.com>
2017-08-14 13:18:18 +0700

    edit - product in backend

--- process/backend/changestatus.php
+++ process/backend/changestatus.php
@@ -11,10 +11,60 @@ if($_GET['status'] == "notcheck"){
   $sql = "UPDATE payment SET status = 'checked' WHERE id='$id'";
   mysql_query($sql,$con) or die("UPDATE ลงตาราง product มีข้อผิดพลาดเกิดขึ้น".
   mysql_error());
+  $count=0;
+  $strSQL = "SELECT * FROM payment JOIN orders_detail ON payment.order_id = orders_detail.order_id WHERE payment.id = $id";
+  $result = mysql_query($strSQL,$con) or die(mysql_error());
+  while($row = mysql_fetch_array($result)){
+      $data[$count][0] = $row['id'];
+      $data[$count][1] = $row['order_id'];
+      $data[$count][2] = $row['account'];
+      $data[$count][3] = $row['datepayment'];
+      $data[$count][4] = $row['amount'];
+      $data[$count][5] = $row['timepayment'];
+      $data[$count][6] = $row['status'];
+      $data[$count][7] = $row['order_detail_id'];
+      $data[$count][8] = $row['product_id'];
+      $data[$count][9] = $row['qty'];
+      $count++;
+  }
+  for($i=0;$i<=sizeof($data);$i++)
+  {
+    $strSQL1 = mysql_query("SELECT * FROM product WHERE product_id = '".$data[$i][8]."'");
+    while($user = mysql_fetch_array($strSQL1)){
+      $pro_num = $user["product_num"] - $data[$i][9];
+    }
+    $strSQL2 = "UPDATE product SET product_num = '".$pro_num."' WHERE product_id = '".$data[$i][8]."'";
+    mysql_query($strSQL2) or die(mysql_error());
+  }
 } else if ($_GET['status'] == "checked") {
   $sql = "UPDATE payment SET status = 'notcheck' WHERE id='$id'";
   mysql_query($sql,$con) or die("UPDATE ลงตาราง product มีข้อผิดพลาดเกิดขึ้น".
   mysql_error());
+  $count=0;
+  $strSQL = "SELECT * FROM payment JOIN orders_detail ON payment.order_id = orders_detail.order_id WHERE payment.id = $id";
+  $result = mysql_query($strSQL,$con) or die(mysql_error());
+  while($row = mysql_fetch_array($result)){
+      $data[$count][0] = $row['id'];
+      $data[$count][1] = $row['order_id'];
+      $data[$count][2] = $row['account'];
+      $data[$count][3] = $row['datepayment'];
+      $data[$count][4] = $row['amount'];
+      $data[$count][5] = $row['timepayment'];
+      $data[$count][6] = $row['status'];
+      $data[$count][7] = $row['order_detail_id'];
+      $data[$count][8] = $row['product_id'];
+      $data[$count][9] = $row['qty'];
+      $count++;
+  }
+  for($i=0;$i<=sizeof($data);$i++)
+  {
+    $strSQL1 = mysql_query("SELECT * FROM product WHERE product_id = '".$data[$i][8]."'");
+    while($user = mysql_fetch_array($strSQL1)){
+      $pro_num = $user["product_num"] + $data[$i][9];
+    }
+    $strSQL2 = "UPDATE product SET product_num = '".$pro_num."' WHERE product_id = '".$data[$i][8]."'";
+    mysql_query($strSQL2) or die(mysql_error());
+  }
 } else if ($_GET['status'] == "error") {
   $sql = "UPDATE payment SET status = 'notcheck' WHERE id='$id'";
   mysql_query($sql,$con) or die("UPDATE ลงตาราง product มีข้อผิดพลาดเกิดขึ้น".
--- process/save_checkout.php
+++ process/save_checkout.php
@@ -1,15 +1,14 @@
 <?php
-session_start();
-
-include("../Env/config.php");
+  session_start();
+  include("../Env/config.php");
 
   $Total = 0;
   $SumTotal = 0;
 
   $strSQL = "
-	INSERT INTO orders (OrderDate,Name,Address,Tel,Email)
-	VALUES
-	('".date("Y-m-d H:i:s")."','".$_POST["txtName"]."','".$_POST["txtAddress"]."','".$_POST["txtTel"]."','".$_POST["txtEmail"]."')
+  INSERT INTO orders (OrderDate,Name,Address,Tel,Email)
+  VALUES
+  ('".date("Y-m-d H:i:s")."','".$_POST["txtName"]."','".$_POST["txtAddress"]."','".$_POST["txtTel"]."','".$_POST["txtEmail"]."')
   ";
   mysql_query($strSQL) or die(mysql_error());
 
@@ -17,39 +16,25 @@ include("../Env/config.php");
 
   for($i=0;$i<=(int)$_SESSION["intLine"];$i++)
   {
-	  if($_SESSION["strProductID"][$i] != "")
-	  {
-			  $strSQL = "
-				INSERT INTO orders_detail (order_id,product_id,qty)
-				VALUES
-				('".$strOrderID."','".$_SESSION["strProductID"][$i]."','".$_SESSION["strQty"][$i]."')
-			  ";
-			  mysql_query($strSQL) or die(mysql_error());
-
-        $strSQL1 = mysql_query("SELECT * FROM product WHERE product_id = '".$_SESSION["strProductID"][$i]."'");
-        while($user = mysql_fetch_array($strSQL1)){
-          $pro_num = $user["product_num"] - $_SESSION["strQty"][$i];
-        }
-
-        $strSQL2 = "UPDATE product SET product_num = '".$pro_num."' WHERE product_id = '".$_SESSION["strProductID"][$i]."'";
-        mysql_query($strSQL2) or die(mysql_error());
-	  }
+    if($_SESSION["strProductID"][$i] != "")
+    {
+  		  $strSQL = "
+  			INSERT INTO orders_detail (order_id,product_id,qty)
+  			VALUES
+  			('".$strOrderID."','".$_SESSION["strProductID"][$i]."','".$_SESSION["strQty"][$i]."')
+  		  ";
+  		  mysql_query($strSQL) or die(mysql_error());
+    }
   }
   for($i=0;$i<=(int)$_SESSION["intLine"];$i++)
   {
-	  if($_SESSION["strProductID"][$i] != "")
-	  {
+    if($_SESSION["strProductID"][$i] != "")
+    {
       $_SESSION["strProductID"][$i] = "";
       $_SESSION["strQty"][$i] = "";
       $_SESSION["intLine"]=0;
     }
   }
-
-mysql_close();
-
-// session_destroy();
-
-echo "<meta http-equiv='refresh' content='1;URL=../page/view_order.php?OrderID=".$strOrderID."'>";
-
-// header("location:finish_order.php?OrderID=".$strOrderID);
+  mysql_close();
+  echo "<meta http-equiv='refresh' content='1;URL=../page/view_order.php?OrderID=".$strOrderID."'>";
 ?>
